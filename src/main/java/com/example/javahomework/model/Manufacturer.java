809d9486b21a90d4e4dbce45a32bc6d67f00fc63
Çağatay Sunal <cagatay.sunal@boer.com.tr>
2022-11-25 13:24:21 +0300

    getManufacturers

---
+++ src/main/java/com/example/javahomework/model/Manufacturer.java
@@ -0,0 +1,22 @@
+package com.example.javahomework.model;
+
+public class Manufacturer {
+    private int _id;
+    private String name;
+
+    public int get_id() {
+        return _id;
+    }
+
+    public void set_id(int _id) {
+        this._id = _id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
