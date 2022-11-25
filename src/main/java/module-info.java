b610abad23c3972eb18fe3544c03efdeafec60a9
Çağatay Sunal <cagatay.sunal@boer.com.tr>
2022-11-25 10:17:02 +0300

    exported com.example.javahomework.model

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,6 +6,7 @@ module com.example.javahomework {
 
     opens com.example.javahomework to javafx.fxml;
     exports com.example.javahomework;
+    exports com.example.javahomework.model;
     exports com.example.javahomework.model.equipment;
     opens com.example.javahomework.model.equipment to javafx.fxml;
 }
